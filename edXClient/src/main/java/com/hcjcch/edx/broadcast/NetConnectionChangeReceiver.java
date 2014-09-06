73d4b54e9e9d060f3465bcde99670308b30fd02d
hcjcch <huangchen@mail.bistu.edu.cn>
2014-09-06 15:09:11 +0800

    add netWork listener

---
+++ edXClient/src/main/java/com/hcjcch/edx/broadcast/NetConnectionChangeReceiver.java
@@ -0,0 +1,28 @@
+package com.hcjcch.edx.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
+
+import com.hcjcch.edx.event.NetworkChangeEvent;
+import com.hcjcch.edx.utils.NetworkJudgment;
+
+import de.greenrobot.event.EventBus;
+
+/**
+ * Created by hcjcch on 2014/9/2.
+ */
+
+public class NetConnectionChangeReceiver extends BroadcastReceiver {
+    private static final String CONNECTIVITY_CHANGE_ACTION = "android.net.conn.CONNECTIVITY_CHANGE";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (TextUtils.equals(action,CONNECTIVITY_CHANGE_ACTION)){
+            boolean isNetworkConnected = NetworkJudgment.isNetworkConnected(context);
+            EventBus.getDefault().post(new NetworkChangeEvent(isNetworkConnected));
+        }
+    }
+}
